021047a7bbb7275b560ee223f4d008ea4b42c91b
Gintautas Miliauskas <gintautas@miliauskas.lt>
2014-04-21 03:09:01 +0200

    Genetic optimization works!

--- Lander.hs
+++ Lander.hs
@@ -1,6 +1,9 @@
 module Lander where
 
+import Data.Function
+import Data.List
 import System.Random
+import System.IO.Unsafe
 import Debug.Trace -- XXX
 
 type Angle = Double
@@ -34,7 +37,7 @@ data Lander = Lander { landerPos :: Vector2D
                      , landerAngleV :: Angle
                      } deriving (Eq, Show)
 
-data ControlPoint = ControlPoint Double Double deriving (Eq, Show)
+data ControlPoint = ControlPoint Double Double deriving (Eq, Ord, Show)
 
 -- Constants
 boundaries = Vector2D 100.0 100.0
@@ -64,7 +67,8 @@ type Score = Double
 fly :: Lander -> [ControlPoint] -> (Lander, Duration)
 fly lander cs = fly' cs 0.0 defaultStep lander
   where
-    fly' ((ControlPoint pa ra):cs) t dt lander = trace (show $ lander) $
+    fly' ((ControlPoint pa ra):cs) t dt lander =
+     --trace (show $ lander) $
      if inBounds boundaries $ landerPos lander
      then let pav = gAcceleration + angleVector (landerAngle lander) pa in
        fly' cs (t+dt) dt $ updateVelocities defaultStep pav ra $ moveLander defaultStep lander
@@ -73,29 +77,93 @@ fly lander cs = fly' cs 0.0 defaultStep lander
 
 calculateScore :: Duration -> Lander -> Score
 calculateScore t (Lander (Vector2D x y) pv r rv) =
-  if not $ inBounds boundaries p' then 0
+  if not $ inBounds boundaries p' then inf
   else len2 (landingPadPosition - p') + len2 pv + r*r  -- TODO: weights
   where p' = Vector2D x 0
+        inf = 1/0
 
 nullControls = (ControlPoint 0.0 0.0) : nullControls
 
 multiplex2 (x1:x2:xs) = (x1, x2):(multiplex2 xs)
 
+createControlPoint :: (Int, Int) -> ControlPoint
+createControlPoint (r1, r2) = ControlPoint pa ra
+  where pa = case r1 of
+          -1 -> 0
+          0 -> 5
+          1 -> 20
+        ra = case r2 of
+          -1 -> -45
+          0 -> 0
+          1 -> 45
+
 randomControls :: StdGen -> [ControlPoint]
 randomControls g =
   let rs = randomRs (-1 :: Int, 1) g
-  in map cp (multiplex2 rs)
-  where cp (r1, r2) = ControlPoint pa ra
-                      where pa = case r1 of
-                              -1 -> 0
-                              0 -> 5
-                              1 -> 15
-                            ra = case r2 of
-                              -1 -> -45
-                              0 -> 0
-                              1 -> 45
+  in map createControlPoint (multiplex2 rs)
+
+type Probability = Double
+
+flightScore :: [ControlPoint] -> Score
+flightScore cs = calculateScore t lander
+  where (lander, t) = fly newLander cs
+
+selection :: (a -> Score) -> [a] -> Int -> [a]
+selection scorer css m = map snd $ take m $ sortBy (compare `on` fst) $ map result css
+  where result cs = (scorer cs, cs)
+
+combine :: Probability -> StdGen -> [a] -> [a] -> [a]
+combine p g (c1:cs1) (c2:cs2) =
+  c : (combine p g' cs1 cs2)
+  where (r, g') = random g
+        c = if r > p then c1 else c2
+
+randomPair :: StdGen -> Int -> (Int, Int, StdGen)
+randomPair g n =
+  if i1 /= i2 then (i1, i2, g'') else randomPair g'' n
+  where (i1, g') = randomR (0, n) g
+        (i2, g'') = randomR (0, n) g'
+
+recombination :: StdGen -> [[ControlPoint]] -> Int -> [[ControlPoint]]
+recombination _ _ 0 = []
+recombination g ccs n =
+  let entry = combine 0.5 g' (ccs !! i1) (ccs !! i2)
+      rest = recombination g' ccs (n - 1)
+  -- in trace (show i1 ++ " " ++ show i2) $
+  in entry : rest
+  where (i1, i2, g') = randomPair g (length ccs - 1)
+
+mutation :: StdGen -> [[ControlPoint]] -> [[ControlPoint]]
+mutation g ccs = map mutate ccs
+  where mutate cs = combine kMutation g cs rnd
+        rnd = randomControls $ g
+
+kPopulation = 200
+kSelected = 30
+kMutation = 0.01
+
+runBatch :: [[ControlPoint]] -> [[ControlPoint]]
+runBatch ccs =
+  let top = selection flightScore ccs kSelected
+      mutated = mutation (unsafePerformIO newStdGen) top
+      new = recombination (unsafePerformIO newStdGen) top (kPopulation - kSelected - kSelected)
+  in top ++ mutated ++ new
+
+initialPopulation :: Int -> [[ControlPoint]]
+initialPopulation n = map (\s -> randomControls $ (unsafePerformIO newStdGen)) [1..n]
+
+geneticSearch :: [[[ControlPoint]]]
+geneticSearch = iterate runBatch (initialPopulation kPopulation)
+
+poolRating :: [[ControlPoint]] -> Score
+poolRating ccs = sum (map flightScore $ take kSelected ccs) / (fromIntegral kSelected)
 
 main :: IO ()
 main =
-  print $ calculateScore t lander
-  where (lander, t) = fly newLander (randomControls $ mkStdGen 1)
+  --print $ map flightScore $ selection flightScore (initialPopulation 100) 10
+  printResult 0 geneticSearch
+  where printResult n (x:xs) = do
+          if (n `mod` 50) == 0
+            then putStrLn $ (show n) ++ ": " ++ (show $ poolRating x) ++ "\n" ++ (show $ fly newLander (head x))
+            else return ()
+          printResult (n+1) xs
